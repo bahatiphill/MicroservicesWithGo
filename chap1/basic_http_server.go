7cce0bb2d9f601e00f43c791633a0803bebe49a5
phill <phill@localhost.localdomain>
2019-09-16 11:28:30 +0200

    added JSON encoding responses

--- chap1/basic_http_server.go
+++ chap1/basic_http_server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type helloworldResponse struct {
+	Message string
+}
 
 func main() {
 
@@ -17,5 +22,11 @@ func main() {
 }
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!\n")
+	response := helloworldResponse{Message: "Helloworld"}
+	data, err := json.Marshal(response)
+
+	if err != nil {
+		panic("Oops, Can't encode the response to JSON")
+	}
+	fmt.Fprint(w, string(data))
 }
