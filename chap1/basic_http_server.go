062a72c8ece1e7dbde71eaea82048238fc00c70f
phill <phill@localhost.localdomain>
2019-09-16 11:44:33 +0200

    Using Encoder to send response directly, which is faster 50% than Mashalling

--- chap1/basic_http_server.go
+++ chap1/basic_http_server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
 
 type helloworldResponse struct {
-	Message string
+	Message string `json:"message"`
 }
 
 func main() {
@@ -23,10 +22,7 @@ func main() {
 
 func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	response := helloworldResponse{Message: "Helloworld"}
-	data, err := json.Marshal(response)
-
-	if err != nil {
-		panic("Oops, Can't encode the response to JSON")
-	}
-	fmt.Fprint(w, string(data))
+	//data, err := json.Marshal(response)
+	encoder := json.NewEncoder(w)
+	encoder.Encode(&response)
 }
